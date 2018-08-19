9203bc7f9ae825d4fb652287d8057ff8bec47437
Hello <31024588+DSep@users.noreply.github.com>
2018-08-19 15:10:50 -0700

    Corrected solution for question 1.

--- Problems/Question1.cpp
+++ Problems/Question1.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
+using namespace std;
 // Find all the multiples of 3 and 5 under 1000
 int main() {
   int max = 1000;
   int ans = 0;
   for (int i = 0; i < max; i++) {
     if (i % 3 == 0 || i % 5 == 0) {
-      ans++;
+      ans += i;
     }
   }
-  return ans;
+  cout << ans << endl;
+  return 0;
 }
